34fb0158c2d80801c659ef70b6c93c757b2ecc7b
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-04-30 12:47:32 +0100

    next-config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
